5dd9fba23514261330feddaf61076c652580785d
schutzec <schutzec@oregonstate.edu>
2024-05-30 02:30:41 -0700

    Enrollments: insert and select complete, searching implemented too

--- controllers/enrollments.js
+++ controllers/enrollments.js
@@ -1,13 +1,81 @@
 var db = require('../database/db_connector');
 
 exports.get_enrollments = (req, res, next) => {
-    // Temporary function for getting courses
-    // TODO: Implement the function to retrieve courses from the database
-    res.send('This is the get_courses function');
+    let enrollments_query;
+
+    if (!req.query.last_name || req.query.last_name === '') {
+        enrollments_query = ` \
+            SELECT \
+            Enrollments.enrollment_id AS id, \
+            Enrollments.status AS status, \
+            Enrollments.grade AS grade, \
+            CONCAT(Users.first_name, ' ', Users.last_name) AS user, \
+            Courses.name AS course \
+            FROM Enrollments \
+            INNER JOIN Courses ON Enrollments.course_id = Courses.course_id \
+            INNER JOIN Users ON Enrollments.user_id = Users.user_id \
+            ORDER BY id; \
+        `;
+    } else {
+        const last_name = req.query.last_name;
+        enrollments_query = ` \
+            SELECT \
+            Enrollments.enrollment_id AS  id, \
+            Enrollments.status AS status, \
+            Enrollments.grade AS grade, \
+            CONCAT(Users.first_name, ' ', Users.last_name) AS user, \
+            Courses.name AS course \
+            FROM Enrollments \
+            INNER JOIN Courses ON Enrollments.course_id = Courses.course_id \
+            INNER JOIN Users ON Enrollments.user_id = Users.user_id \
+            WHERE Users.last_name LIKE '${last_name}%' \
+            ORDER BY id; \
+        `;
+    }
+
+    let users_query = 'SELECT Users.user_id, Users.first_name, Users.last_name FROM Users';
+
+    let courses_query = 'SELECT Courses.course_id, Courses.name FROM Courses;';
+
+    db.pool.query(enrollments_query, (error, rows, fields) => {
+        let enrollments = rows;
+        db.pool.query(users_query, (error, rows, fields) => {
+            let users = rows;
+            db.pool.query(courses_query, (error, rows, fields) => {
+                let courses = rows;
+                return res.render('pages/enrollments', {data: enrollments, users: users, courses: courses});
+            })
+        })
+    })
 };
 
 exports.add_enrollment = (req, res, next) => {
-    // Temporary function for adding a course
-    // TODO: Implement the function to add a course to the database
-    res.send('This is the add_course function');
+    const { status, grade, user_id, course_id } = req.body;
+
+    const add_enrollments_query = ` \
+        INSERT INTO Enrollments (status, grade, user_id, course_id) \
+        VALUES (?, ?, ?, ?); \
+    `;
+
+    const new_enrollment_data = [status, grade, user_id, course_id];
+
+    db.pool.query(add_enrollments_query, new_enrollment_data, (error, results) => {
+        const new_enrollment_id = results.insertId;
+        let add_enrollment_query = ` \
+            SELECT \
+            Enrollments.enrollment_id AS  id, \
+            Enrollments.status AS status, \
+            Enrollments.grade AS grade, \
+            CONCAT(Users.first_name, ' ', Users.last_name) AS user, \
+            Courses.name AS course \
+            FROM Enrollments \
+            INNER JOIN Courses ON Enrollments.course_id = Courses.course_id \
+            INNER JOIN Users ON Enrollments.user_id = Users.user_id \
+            WHERE Enrollments.enrollment_id = ?; \
+        `;
+
+        db.pool.query(add_enrollment_query, [new_enrollment_id], (error, rows, fields) => {
+            return res.json(rows);
+        })
+    })
 };
---
+++ public/js/enrollments/add_enrollment.js
@@ -0,0 +1,93 @@
+// Get the objects we need to modify
+let add_enrollments_form = document.getElementById('add_enrollments_form');
+
+// Modify the objects we need
+add_enrollments_form.addEventListener("submit", function (e) {
+    
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let input_status = document.getElementById("input_enrollments_status_add");
+    let input_grade = document.getElementById("input_enrollments_grade_add");
+    let input_user = document.getElementById("input_enrollments_user_add");
+    let input_course = document.getElementById("input_enrollments_course_add");
+
+    // Get the values from the form fields
+    let status_value = input_status.checked ? 1 : 0;
+    let grade_value = input_grade.value;
+    let user_value = input_user.value;
+    let course_value = input_course.value;
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        status: status_value,
+        grade: grade_value,
+        user_id: user_value,
+        course_id: course_value
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "/enrollments/add/", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            let new_enrollment = JSON.parse(xhttp.response)[0];
+            // Add the new data to the table
+            add_row_to_enrollments(new_enrollment);
+
+            // Clear the input fields for another transaction
+            input_status.value = '';
+            input_grade.value = '';
+            input_user.value = '';
+            input_course.value = '';
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+// Creates a single row from an Object representing a single record from 
+// bsg_people
+add_row_to_enrollments = (data) => {
+
+    // Get a reference to the current table on the page and clear it out.
+    let enrollments_table = document.getElementById("enrollments_table");
+
+    // Create a row and 5 cells
+    let row = document.createElement("TR");
+    let id_cell = document.createElement("TD");
+    let status_cell = document.createElement("TD");
+    let grade_cell = document.createElement("TD");
+    let user_cell = document.createElement("TD");
+    let course_cell = document.createElement("TD");
+
+    // Fill the cells with correct data
+    id_cell.innerText = data.id;
+    status_cell.innerText = data.status;
+    grade_cell.innerText = data.grade;
+    user_cell.innerText = data.user;
+    course_cell.innerText = data.course;
+
+    // Add the cells to the row 
+    row.appendChild(id_cell);
+    row.appendChild(status_cell);
+    row.appendChild(grade_cell);
+    row.appendChild(user_cell);
+    row.appendChild(course_cell);
+
+    // Add a row attribute so the deleteRow function can find a newly added row
+    row.setAttribute('data-value', data.id);
+    
+    // Add the row to the table
+    enrollments_table.appendChild(row);
+}
